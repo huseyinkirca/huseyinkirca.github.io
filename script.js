2e70263b8c949f1a07fff16c256f6d51776a4a53
huseyinkirca <66334275+huseyinkirca@users.noreply.github.com>
2021-05-31 10:31:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCijBP0I09SkcdcaCKLfbD1Q3hLNs05UZY",
-    authDomain: "realtime-chat-42ee9.firebaseapp.com",
-    projectId: "realtime-chat-42ee9",
-    storageBucket: "realtime-chat-42ee9.appspot.com",
-    messagingSenderId: "326490386953",
-    appId: "1:326490386953:web:2684f1c41ae24d1709bed8",
+    apiKey: "AIzaSyApVd25nOOHOImk3ngM-GHwhXUBzic8nmc",
+    authDomain: "realtime-chat-96508.firebaseapp.com",
+    projectId: "realtime-chat-96508",
+    storageBucket: "realtime-chat-96508.appspot.com",
+    messagingSenderId: "84516305780",
+    appId: "1:84516305780:web:d9162e45cb943820990a21",
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
